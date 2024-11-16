5ac8904d748cb966bb2e0325836341f4ddc7a97a
Efemena Johnson <163689806+frankoceansbride@users.noreply.github.com>
2024-11-16 16:01:22 -0500

    Update predict_data.py

--- predict_data.py
+++ predict_data.py
@@ -7,8 +7,7 @@ model = joblib.load('healthcare_model.joblib')
 scaler = joblib.load('scaler.joblib')
 
 feature_names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
-new_data = np.array([[52, 1, 0, 125, 212, 0, 1, 168, 0, 1.0, 2, 2, 3]])  # Replace with actual feature values
-
+new_data = np.array([[52, 1, 0, 125, 212, 0, 1, 168, 0, 1.0, 2, 2, 3]]) 
 # Convert new_data to a DataFrame with feature names
 new_data_df = pd.DataFrame(new_data, columns=feature_names)
 
